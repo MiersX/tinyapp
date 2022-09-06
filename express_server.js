c3cbb9e6f1537c90c909cf489b88cd11ce1a8ed3
MiersX <michael.muirhead123@gmail.com>
2022-09-06 20:48:11 +0000

    Installed nodemon and morgan packages into the express server, update package.json with script to launch nodemon

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; 
+const morgan = require("morgan");
+app.use(morgan("dev"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}));
 
